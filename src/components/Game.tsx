5b4126bbf14fa688e749800bdaaf729ef4a9881f
Eloisa Borromeo <eefborromeo@gmail.com>
2024-12-06 12:15:10 +0800

    creates game page

---
+++ src/components/Game.tsx
@@ -0,0 +1,54 @@
+import mountains from '../assets/mountains.svg';
+import secondaryBg from '../assets/secondary-bg.svg';
+import foreground from '../assets/foreground.svg';
+import GameControls from './GameControls';
+import GameBoard from './GameBoard';
+import Congratulations from './Congratulations';
+import { useGame } from '../context/GameContext';
+
+export default function Game() {
+	const { isGameFinished } = useGame();
+	return (
+		<>
+			<div className="w-full bg-[#1C1A6B]">
+				<div className="w-full min-h-screen relative grid place-items-centers bg-[#AACBEC]">
+					<div
+						className="absolute inset-0 z-0"
+						style={{
+							backgroundImage: `url(${mountains})`,
+							backgroundPosition: 'bottom',
+							backgroundSize: 'contain',
+							backgroundRepeat: 'no-repeat',
+						}}></div>
+					<div
+						className="absolute inset-0 z-10"
+						style={{
+							backgroundImage: `url(${secondaryBg})`,
+							backgroundPosition: 'bottom',
+							backgroundSize: 'contain',
+							backgroundRepeat: 'no-repeat',
+						}}></div>
+					<div
+						className="absolute inset-0 z-20"
+						style={{
+							backgroundImage: `url(${foreground})`,
+							backgroundPosition: 'bottom',
+							backgroundSize: 'contain',
+							backgroundRepeat: 'no-repeat',
+						}}></div>
+
+					<div className="w-9/12 mx-auto relative inset-0 z-30 text-center mt-10">
+						{!isGameFinished ? (
+							<>
+								<GameControls />
+								<GameBoard />
+							</>
+						) : (
+							<Congratulations />
+						)}
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
